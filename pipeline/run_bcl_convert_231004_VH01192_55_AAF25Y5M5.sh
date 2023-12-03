0a0d5d250031bab537ddb0319ab36cf6cd6e271c
Jason Stajich <jasonstajich.phd@gmail.com>
2023-12-03 14:33:35 -0800

    convert these to single job file

--- pipeline/run_bcl_convert_231004_VH01192_55_AAF25Y5M5.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash -l
-#SBATCH -p short -c 48 --mem 64gb -C xeon 
-
-module load bclconvert
-# see this on google cloud
-# gs://stajichlab-ecdre/Amplicon/231004_VH01192_55_AAF25Y5M5.NextSeq2000_raw
-
-bcl-convert --bcl-input-directory 231004_VH01192_55_AAF25Y5M5.NextSeq2000_raw --output-directory 231004_VH01192_55_AAF25Y5M5 \
-	--force --sample-sheet ECDRE/231004_VH01192_55_AAF25Y5M5.csv
--- pipeline/run_bcl_convert_231129_VH01192_63_AAFFG3CM5.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash -l
-#SBATCH -p short -c 48 --mem 64gb -C xeon 
-
-module load bclconvert
-# see this on google cloud
-# gs://stajichlab-ecdre/Amplicon/231129_VH01192_63_AAFFG3CM5.NextSeq2000_raw
-
-bcl-convert --bcl-input-directory 231129_VH01192_63_AAFFG3CM5.NextSeq2000_raw --output-directory 231129_VH01192_63_AAFFG3CM5 \
-	--force --sample-sheet ECDRE/231129_VH01192_63_AAFFG3CM5.csv
