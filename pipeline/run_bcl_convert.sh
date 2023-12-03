f175fffd67895d8847351e308fd1e0ac6a607b69
Jason Stajich <jasonstajich.phd@gmail.com>
2023-12-03 14:33:42 -0800

    convert these to single job file

---
+++ pipeline/run_bcl_convert.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash -l
+#SBATCH -p short -c 64 --mem 128gb -C xeon --out logs/bcl-convert.%A_%a.log
+module load bclconvert
+# see this on google cloud
+# gs://stajichlab-ecdre/Amplicon/
+CPU=64
+
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ -z $N ]; then
+    N=$1
+    if [ -z $N ]; then
+        echo "Need an array id or cmdline val for the job"
+        exit
+    fi
+fi
+
+SHEET=flowcells.csv
+BASE=/bigdata/stajichlab/shared/projects/ECDRE/Amplicon/raw
+OUT=/bigdata/stajichlab/shared/projects/ECDRE/Amplicon/data/UF_UCR_ECDRE_Trials/Flowcells
+mkdir -p $OUT
+IFS=,
+sed -n ${N}p $SHEET | while read FLOWCELL DESC NOTE
+do
+	bcl-convert --bcl-input-directory $BASE/$FLOWCELL.NextSeq2000_raw --output-directory $OUT/$FLOWCELL \
+		--no-lane-splitting TRUE --bcl-num-conversion-threads $CPU \
+	  	--force --sample-sheet $(realpath ECDRE/$FLOWCELL.csv)
+done
