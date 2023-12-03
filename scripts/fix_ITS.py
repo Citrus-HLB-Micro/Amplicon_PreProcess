e17ccdec9eeb5007862aa6925bec895aa2e98b83
Jason Stajich <jasonstajich.phd@gmail.com>
2023-12-03 14:33:58 -0800

    dump ITS conversion

---
+++ scripts/fix_ITS.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from Bio.Seq import Seq
+import sys
+import csv
+with open(sys.argv[1],"r") as fh:
+    reader = csv.reader(fh,delimiter="\t")
+    outcsv = csv.writer(sys.stdout,delimiter=",")
+    header= next(reader)
+    outcsv.writerow(['Sample_ID','Index'])
+    for row in reader:
+        samp = row[1]
+        barcode = Seq(row[2])
+        if samp.endswith("ITS"):
+            barcode = barcode.reverse_complement()
+        outcsv.writerow([samp,barcode])
