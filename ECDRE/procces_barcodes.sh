7a623407368b7a67266456ee450c9fb5fe8265ab
Jason Stajich <jasonstajich.phd@gmail.com>
2024-02-09 16:07:18 -0800

    update for automation, fix Jessica mistakes in some of barcodes

---
+++ ECDRE/procces_barcodes.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash -l
+
+for pool in $(seq 3)
+do
+	for type in ITS 16S
+	do
+		FILE=Pool_$pool.$type.tsv
+		CSV=Pool_$pool.$type.csv
+		python ../scripts/fix_barcodes.py $FILE > $CSV
+		perl -i -p -e "s/^(Pos|Neg|NAA)/\$1_Pool$pool/" $CSV
+	done
+done
+
