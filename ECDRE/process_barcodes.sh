8add0826903639110a1ef3369b33177738f9cdba
Jason Stajich <jasonstajich.phd@gmail.com>
2024-04-30 12:42:11 -0700

    fix pooling name

--- ECDRE/process_barcodes.sh
+++ ECDRE/process_barcodes.sh
@@ -8,7 +8,7 @@ do
 		CSV=Pool_$pool.$type.csv
 		python ../scripts/fix_barcodes.py $FILE > $CSV
 		dos2unix $CSV
-		perl -i -p -e "s/^(Pos|Neg|NAA)-/\$1-Pool$pool-/" $CSV
+		perl -i -p -e "s/^(Pos|Neg|NAA)[-_]/\$1-Pool$pool-/" $CSV
 	done
 done
 
